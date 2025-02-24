3436c89c36b9182ccabdaebd671aa551000daa4e
maruffirdaus <maruffirdaus@outlook.com>
2025-02-24 09:34:57 +0700

    Fix daily reminder

--- lib/service/local_notification_service.dart
+++ lib/service/local_notification_service.dart
@@ -80,7 +80,7 @@ class LocalNotificationService {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
 
     tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, 11, 00);
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 11);
 
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
